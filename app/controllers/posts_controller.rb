a11f6771185421bfcd5440f57f25cfa5f7cd5105
nakayamahiroshi164 <box.xx164@gmail.com>
2023-09-18 14:57:52 +0900

    JSでレスポンスを処理する

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,11 +3,7 @@ class PostsController < ApplicationController
     @posts = Post.order(id: "DESC")
   end
 
-  def new
-  end
-
   def create
-    binding.pry
     post = Post.create(content: params[:content])
     render json:{ post: post }
   end
